c2f375a6b551c16aaf158725dc5ddf117d948438
YerassylMurat <100864777+YerassylMurat@users.noreply.github.com>
2022-04-02 14:39:32 +0600

    Add files via upload

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 /*===========================*/
 $a=5;
 $b=7;
-if($a>2 && $a<11 && $b>6 && $b<14){echo "True","<br>";}
+if($a>2 && $a<11 && $b>6 || $b<14){echo "True","<br>";}
 else { echo "False ","<br>";}
 
 echo $a+$b,"<br>";
@@ -12,7 +12,7 @@ echo "<br>";
 
 /*===========================*/
 $month=11;
-if($month == 12 || $month<=2){echo "Winter","<br>";}
+if($month == 12 || $month<=2 && $month>0){echo "Winter","<br>";}
 elseif($month >=3 && $month <=5){echo "Spring","<br>";}
 elseif($month >=6 && $month <=8){echo "Summer","<br>";}
 elseif($month >=9 && $month <=11){echo "Autumn","<br>";}
